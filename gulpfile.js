520e9513ead21b4549bde2daa9dbe5c43d4e324f
Tiago Antonio Cominesi <tcominesi@yahoo.com.br>
2025-03-26 14:17:29 -0300

    Incluido fontes

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
     //pegar os arquivos fontes
@@ -9,7 +10,16 @@ function styles() {
     .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
+function images() {
+    //pegar os arquivos fontes
+    return gulp.src('./src/images/**/*')
+    .pipe(imagemin())
+    //mandar esses arquivos para uma pasta
+    .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(styles, images);
+
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
