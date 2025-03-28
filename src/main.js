bd777109ea002b08379221293ef527364aaf25e7
Tiago Antonio Cominesi <tcominesi@yahoo.com.br>
2025-03-28 10:16:06 -0300

    Atualização do site

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
@@ -12,7 +13,19 @@ document.addEventListener('DOMContentLoaded', function(){
         })
     }
 
+    // Seção FAQ, accordion
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
+
 })
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
